ec656e10a39bd9c0a7fa49ccf655ce6ce343b9c7
JGmbr169 <justine.gambier.pro@gmail.com>
2024-07-22 15:14:42 +0200

    deploy pagination on catalog products

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -5,15 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Product;
 
 #[Route('/catalog', name: 'app_catalog')]
 class CatalogController extends AbstractController
 {
     #[Route('/', name: '_index')]
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator, EntityManagerInterface $entityManager): Response
     {
+        $page_range = $this->getParameter('knp_paginator.page_range');
+
+        $datas = $entityManager->getRepository(Product::class)->findAll();
+
+        $products = $paginator->paginate(
+            $datas, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            $page_range // Nombre de résultats par page
+        );
+
         return $this->render('catalog/index.html.twig', [
             'controller_name' => 'CatalogController',
+            'products' => $products
         ]);
     }
 
