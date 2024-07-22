67ff4ec109bcae8522f8487cb9f6de92133f54a7
JGmbr169 <justine.gambier.pro@gmail.com>
2024-07-22 14:07:43 +0200

    add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Product;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 15; $i++) {
+            $product = new Product();
+            $product->setName($faker->word);
+            $product->setDescription($faker->sentence);
+            $product->setPrice($faker->randomFloat(2, 1, 1000));
+            $product->setActive($faker->boolean());
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
